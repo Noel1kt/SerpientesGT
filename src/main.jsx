a9b92cec7b59f25b6c9319403c65ef6b2e0e7d3e
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-11 11:53:14 -0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <button>Hola baton</button>
+    <button>Hola baton</button>
+  </React.StrictMode>,
+)
