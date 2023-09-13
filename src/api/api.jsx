f8ac17765aad4fa6b5b765645447f49017b49c74
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-12 19:45:14 -0600

    await

--- src/api/api.jsx
+++ src/api/api.jsx
@@ -1,9 +1,11 @@
 import axios from "axios" // axios es una librería para hacer peticiones
 
-const api = axios.create({
+export const api = axios.create({
     baseURL: "http://localhost:5000"
 })
 
 export const buscar = async (url, setData) => {
     const respuesta = await api.get(url)
+    setData(respuesta.data)
+    
 }
