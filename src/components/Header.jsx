a4dbd92a561f2c4c777971f765d935f1910e8d5b
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-14 19:33:47 -0600

    add objets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,17 +6,12 @@ export const Header = () => {
 
     return(
         <header className="Header">
-            <div>
-                <a href="">
-                    <img className="Image" src="" alt="" />
-                </a>
-            </div>
             <nav className="nav">
                 <ul className="container-items">
-                    <li><Link to="/">Todas las especies</Link></li>
-                    <li><Link to="/familia/:id">Familias</Link></li>
-                    <li><Link to="/Menu">Cóm usar la guía</Link></li>
-                    <li><Link to="">Sobre los autores</Link></li>
+                    <li><Link className="LinkLi" to="/">Todas las especies</Link></li>
+                    <li><Link className="LinkLi" to="/familia/:id">Familias</Link></li>
+                    <li><Link className="LinkLi" to="/Menu">Cóm usar la guía</Link></li>
+                    <li><Link className="LinkLi" to="">Sobre los autores</Link></li>
                 </ul>
             </nav>
             
