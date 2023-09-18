92aec18dd9ffc341b094638ada070151a8adfafb
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-18 17:44:06 -0600

    styled content

--- src/components/CardInfo.jsx
+++ src/components/CardInfo.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { useParams, useNavigate } from "react-router-dom"
 import { buscar } from "../api/api"
+import "../assets/css/cardInfo.css"
 
 export const Card = ({url}) => {
 
@@ -16,17 +17,20 @@ export const Card = ({url}) => {
         })
     },[id])
     return(
-      <main>
+      <main className="cardInfo">
         <div>
-         {card.img}
+          <img 
+          className="imgCardInfo"
+          src={card.img} alt="serpiente"
+          style={{ width: '600px', height: '450px' }} /> 
         </div>
-        <div>
-          <h2>NOMBRE COMÚN: {card.nombreComun}</h2>
-          <h2>NOMBRE CIENTÍFICO: {card.nombreCientifico}</h2>
-          <h2>DESCRIPCIÓN: {card.descripcion}</h2>
-          <h2>DISTRIBUCIÓN EN GUATEMALA: {card.distribucionEnGuatemala}</h2>
-          <h2>HISTORIA NATURAL: {card.historiaNatural}</h2>
-          <h2>VENENOSIDAD: {card.venenosidad}</h2>
+        <div className="infoContainer">
+          <h2 className="textcontainer">NOMBRE COMÚN:<p>{card.nombreComun}</p></h2>
+          <h2 className="textcontainer">NOMBRE CIENTÍFICO: <p>{card.nombreCientifico}</p></h2>
+          <h2 className="textcontainer">DESCRIPCIÓN:<p>{card.descripcion}</p></h2>
+          <h2 className="textcontainer">DISTRIBUCIÓN EN GUATEMALA:<p>{card.distribucionEnGuatemala}</p></h2>
+          <h2 className="textcontainer">HISTORIA NATURAL:<p>{card.historiaNatural}</p></h2>
+          <h2 className="textcontainer">VENENOSIDAD:<p>{card.venenosidad}</p></h2>
         </div>
         
       </main>
