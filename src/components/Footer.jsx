c38c5c553b3d679da387882c3250666dd7c0ed34
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-20 21:04:05 -0600

    styled footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,14 +3,18 @@ import "../assets/css/footer.css"
 export const Footer = () => {
     return(
     <div className="footer">
-        <div>
-            <img src="" alt="" />
+        <div className="logoFooter">
+            <img src="../img/snakes.png" alt="" />
         </div>
         <ul>
-            <li></li>
+            <li>Guía orginal</li>
+            <li>Guía orginal</li>
+            <li>Guía orginal</li>
+            <li>Guía orginal</li>
         </ul>
         <form action="">
-            
+            <input type="text" />
+            <input type="text" />
         </form>
         
     </div>
