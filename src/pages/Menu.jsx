a8cb6ead2aa59844ad62af5c0cc36d7ba0a39cdd
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-27 17:45:57 -0600

    home

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,10 +1,27 @@
 export const Menu = () => {
     return(
-        <section>
-            
-            <img src="https://aprende.guatemala.com/wp-content/uploads/2021/07/Serpiente-cantil-de-pesta%C3%B1as-en-Guatemala.jpg" alt="" />
-            <h1>Serpientes de Guatemala</h1>
-      
-        </section>
+        <>
+            <section>
+                <h1>Cómo usar esta Guía</h1>
+                <p>Este libro está organizado de tal manera que las fotografías de las especies de serpientes, pueden usarse como referencia para comparar especímenes observados
+                    o capturados en el campo, y así determinar la especie en cuestión. En la página
+                    opuesta a cada fotografía, se presenta la siguiente información:</p>
+            </section>
+            <ul>
+                <li><h3>NOMBRE COMÚN</h3> <p>que es el nombre que se usa en el campo.</p></li>
+                <li><h3>OMBRE CIENTÍFICO</h3> <p>que es el nombre que utilizan los científicos que estudian animales.</p></li>
+                <li><h3>DESCRIPCIÓN</h3> <p>que es la información sobre la forma, tamaño, color y rasgos distintivos de la serpiente.</p></li>
+                <li><h3>DISTRIBUCIÓN EN GUATEMALA</h3> <p>que señala los departamentos o regiones en los que ocurre la especie en el país.</p></li>
+                <li><h3>HISTORIA NATURAL</h3> <p> donde se explica la forma en que vive la serpiente.</p></li>
+                <li><h3>VENENOSIDAD</h3> <p>  que indica qué tan débil o fuerte es el veneno de la serpiente, según el siguiente criterio:
+                        <li><h3>No venenosa:</h3> <p>La serpiente no produce ni inyecta veneno.</p></li>
+                        <li><h3>Levemente venenosa:</h3> <p> El veneno puede producir inflamación leve únicamente en el sitio de la mordedura.</p></li>
+                        <li><h3>Moderadamente venenosa:</h3> <p>El veneno puede causar inflamación y daño nervioso pero no la muerte de la persona.</p></li>
+                        <li><h3>Venenosa:</h3> <p>El veneno puede causar la muerte de la persona. Requiere tratamiento hospitalario.</p></li>
+                    
+                    </p></li>
+            </ul>
+        </>
+        
     )
 }
