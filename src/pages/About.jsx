48fe1fc3958e381ce59b98548f2374e6b2ea8507
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-28 16:21:51 -0600

    about content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,24 +1,40 @@
+import "../assets/css/About.css"
+
 export const About = () => {
     return( <>
-        <section className="cueg-section">
-            <h1 className="cueg-title">Sobre los autores</h1>
-            <p className="cueg-paragraph">Este libro está organizado de tal manera que las fotografías de las especies de serpientes, pueden usarse como referencia para comparar especímenes observados
-                o capturados en el campo, y así determinar la especie en cuestión. En la página
-                opuesta a cada fotografía, se presenta la siguiente información:</p>
+        <section className="about-section">
+            <h1 className="about-title">Sobre los autores</h1>
+            <p className="about-paragraph">Esta web contiene la información del libro "Serpientes de Guatemala: Guía para identificación de especies"
+            de Dennis Guerra Centeno, Héctor Fuentes Rousselin y David Morán Villatoro. Este libro está diseñado para ser utilizado como una guía rápida para identificar
+            la mayoría de las especies de serpientes de Guatemala. Adicionalmente, el texto presenta una síntesis de la información científica disponible sobre las especies
+            abordadas.
+            Además de permitir el reconocimiento de especies, esta guía puede ser un auxiliar
+            en actividades de docencia, educación e investigación y constituye una referencia
+            de la diversidad de uno de los grupos representativos de la biota de Guatemala.
+            Esta primera edición incluye 78 especies nativas, presentadas en orden alfabético
+            según la familia y el nombre científico.
+            La fotografía de cada especie de serpiente está acompañada de la siguiente información: a. nombre común, b. nombre científico, c. descripción de la especie,
+            d. distribución en Guatemala, e. historia natural y f. grado de venenosidad.
+            Esta obra reúne el trabajo de campo y documental de más de cinco años. Todas las
+            fotografías fueron tomadas por los autores.
+            </p>
         
-            <ul className="cueg-ul">
-                <li><h3>NOMBRE COMÚN </h3> <p> es el nombre que se usa en el campo.</p></li>
-                <li><h3>NOMBRE CIENTÍFICO</h3> <p> es el nombre que utilizan los científicos que estudian animales.</p></li>
-                <li><h3>DESCRIPCIÓN</h3> <p> es la información sobre la forma, tamaño, color y rasgos distintivos de la serpiente.</p></li>
-                <li><h3>DISTRIBUCIÓN EN GUATEMALA</h3> <p> señala los departamentos o regiones en los que ocurre la especie en el país.</p></li>
-                <li><h3>HISTORIA NATURAL</h3> <p> donde se explica la forma en que vive la serpiente.</p></li>
-                <li><h3>VENENOSIDAD</h3> <p> indica qué tan débil o fuerte es el veneno de la serpiente, según el siguiente criterio:</p></li>
-                <ul className="cueg-veneno">
-                    <li><h3>No venenosa:</h3> <p>La serpiente no produce ni inyecta veneno.</p></li>
-                    <li><h3>Levemente venenosa:</h3> <p> El veneno puede producir inflamación leve únicamente en el sitio de la mordedura.</p></li>
-                    <li><h3>Moderadamente venenosa:</h3> <p>El veneno puede causar inflamación y daño nervioso pero no la muerte de la persona.</p></li>
-                    <li><h3>Venenosa:</h3> <p>El veneno puede causar la muerte de la persona. Requiere tratamiento hospitalario.</p></li>
-                </ul>
+            <ul className="about-ul">
+                <li><h3>Dennis Guerra Centeno </h3> <p> es Médico Veterinario por la Universidad de San Carlos de
+                        Guatemala, Magister Scientie en Conservación y Manejo de Vida Silvestre por el Instituto Internacional en Conservación y Manejo de Vida Silvestre, UNA, Costa Rica
+                        y candidato a Doctor en Investigación Social por la Universidad Panamericana de
+                        Guatemala. Actualmente se desempeña como coordinador de la Unidad de Vida
+                        Silvestre y de la Maestría en Ciencias en Manejo de Vida Silvestre en la Facultad de
+                        Medicina Veterinaria y Zootecnia de la Universidad de San Carlos de Guatemala. Ha
+                        realizado investigaciones sobre ecología de reptiles y anfibios, accidente ofídico y
+                        hematología y química sérica de vertebrados. msc.dennisguerra@gmail.com</p></li>
+                <li><h3>Héctor Fuentes Rousselin</h3> <p> es Médico Veterinario por la Universidad de San Carlos
+                        de Guatemala y candidato a la Maestría Internacional en Biología de la Conservación por la Universidad Internacional de Andalucía, España. Actualmente es profesor de la Unidad de Vida Silvestre de la Facultad de Medicina Veterinaria y Zootecnia de la Universidad de San Carlos de Guatemala. Ha realizado investigaciones
+                        sobre ecología de reptiles y anfibios. erhfuentes@yahoo.com</p></li>
+                <li><h3>David Morán Villatoro</h3> <p> es Médico Veterinario y Maestro en Ciencias en Manejo de
+                        Vida Silvestre por la Universidad de San Carlos de Guatemala. Actualmente es investigador en la Universidad Del Valle de Guatemala. Ha realizado investigaciones
+                        sobre ecología de reptiles y anfibios, así como sobre enfermedades emergentes y
+                        salud ecosistémica. jodamv@yahoo.com</p></li>
                         
             </ul>
         </section>
