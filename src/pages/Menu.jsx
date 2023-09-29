22bece47cb5d26c4c8f0c27feb47e22f9d01dd88
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-09-29 16:27:48 -0600

    content page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,6 +4,28 @@ export const Menu = () => {
     return(
         <>
             <section className="cueg-section">
+            <h1 className="cueg-title">INTRODUCCIÓN</h1>
+                <p className="cueg-paragraph">Guatemala es un país abundante en recursos naturales y es uno de los puntos más
+                    importantes de diversidad de especies de flora y fauna en Mesoamérica. En el territorio nacional están presentes 14 zonas de vida (De La Cruz 1982). Esta composición compleja del paisaje, permite ese variado ensamble de especies pues
+                    provee innumerables nichos ecológicos. Uno de los grupos de vertebrados más
+                    diverso en el país es el de los reptiles, con 386 especies (Acevedo 2006) y, dentro
+                    de este taxón, las serpientes son las más representativas con 134 especies.
+                    Cada especie de serpiente utiliza los recursos de manera particular. Es por eso
+                    que existen serpientes que se alimentan de roedores, otras que lo hacen de aves
+                    o lagartijas, algunas comen babosas y caracoles, otras ciempiés, arañas e incluso
+                    algunas consumen otras serpientes. Hay especies de hábitos nocturnos y otras de
+                    hábitos diurnos. Algunas especies son terrestres, otras arborícolas, algunas acuáticas y otras cavadoras.
+                    Alrededor de las serpientes existe un miedo infundado y una serie de mitos y
+                    creencias, la mayoría erróneas. Por ejemplo, se cree que las serpientes pueden
+                    “picar” con la cola, que silban, que “azotan” o “chicotean”, que persiguen a la gente, etcétera.
+                    Muchas personas, sobre todo en el campo, consideran que la mayoría de serpientes son venenosas o, de alguna manera, peligrosas o nocivas para el humano. Esta
+                    creencia podría conducir a la matanza, disminución y hasta extinción local de muchas especies.
+                    La causa principal del miedo injustificado hacia las serpientes es el desconocimiento general de la población guatemalteca sobre estos animales.
+                    La presente guía tiene como objetivo ser un instrumento que facilite la identificación de especies y que proporcione datos sobre la distribución y los hábitos de las
+                    especies en Guatemala, así como los datos sobre su venenosidad.
+                    Esperamos que esta información contribuya a la educación de los guatemaltecos,
+                    a la disminución de los accidentes por mordedura de serpiente y a la conservación
+                    de nuestra riqueza de especies de fauna.</p>
                 <h1 className="cueg-title">Cómo usar esta Guía</h1>
                 <p className="cueg-paragraph">Este libro está organizado de tal manera que las fotografías de las especies de serpientes, pueden usarse como referencia para comparar especímenes observados
                     o capturados en el campo, y así determinar la especie en cuestión. En la página
@@ -21,9 +43,65 @@ export const Menu = () => {
                         <li><h3>Levemente venenosa:</h3> <p> El veneno puede producir inflamación leve únicamente en el sitio de la mordedura.</p></li>
                         <li><h3>Moderadamente venenosa:</h3> <p>El veneno puede causar inflamación y daño nervioso pero no la muerte de la persona.</p></li>
                         <li><h3>Venenosa:</h3> <p>El veneno puede causar la muerte de la persona. Requiere tratamiento hospitalario.</p></li>
-                    </ul>
-                            
+                    </ul>         
                 </ul>
+                <h1 className="cueg-title">ACCIDENTE OFÍDICO
+                    (ENVENENAMIENTO POR SERPIENTE)</h1>
+                <p className="cueg-paragraph">Según la especie, los venenos de serpiente, pueden tener un efecto desde leve,
+                    hasta mortal. La función primaria de estas sustancias es inmovilizar a la presa lo
+                    antes posible.
+                    Los casos de mordedura de serpiente en Centroamérica son causados principalmente por víboras y en casos raros por serpientes coral (Gutiérrez 2010).
+                    La barba amarilla (Bothrops asper) y la cascabel (Crotalus spp) son las especies
+                    que causan la mayoría de casos graves de envenenamiento por serpiente en
+                    Centroamérica.
+                    Las víboras poseen dos colmillos móviles en la parte anterior de la boca. Estos
+                    colmillos poseen un conducto interior a través del cual es inyectado el veneno.
+                    Los envenenamientos causados por víboras se caracterizan por alteraciones en
+                    el sitio de la mordedura, incluyendo edema, ampollas, hemorragia y muerte de
+                    tejido, a menudo asociada con infección (Gutiérrez 2010). En casos moderados a
+                    severos, aparecen alteraciones en otras partes del cuerpo, tales como sangrado,
+                    falta de coagulación, ataque cardíaco y fallo renal agudo. Después del diagnóstico
+                    inicial basado en la evaluación de los síntomas y signos, el tratamiento consiste en
+                    la aplicación intravenosa del antídoto adecuado (suero polivalente antibotrópico
+                    o anticoral).
+                    El éxito de la terapia con sueros antiofídicos se basa
+                    en la pronta aplicación. El objetivo
+                    de los anticuerpos
+                    contenidos en el
+                    suero, es evitar
+                    los daños y no
+                    revertirlos.
+                    Los sueros antiofídicos deben ser
+                    aplicados únicamente por personal capacitado y en un hospital o centro de salud, pues pueden producirse reacciones adversas al
+                    suero, que se presentan en personas cuyo organismo reacciona cuando entra en
+                    contacto con el suero de caballo o el de oveja.
+                    El número de dosis (viales) de suero antiofídico a aplicar, varía en cada caso, de
+                    acuerdo a la severidad de las lesiones.
+                    No todos los centros de salud y hospitales a nivel nacional, cuentan con suficientes
+                    dosis de sueros antiofídicos. Las personas que por alguna razón estén más propensas a sufrir de un encuentro con serpientes venenosas, podrían adquirir varias
+                    dosis de antídoto y llevarlo a un hospital para su aplicación, en caso de sufrir un
+                    accidente ofídico.
+                </p>
+                <h1 className="cueg-title">RECOMENDACIONES PARA LA PREVENCIÓN DE MORDEDURAS DE SERPIENTE</h1>
+                <p className="cueg-paragraph">Observe su camino. Procure mantenerse dentro de los senderos. No camine sin
+                    iluminación por el monte durante la noche. Utilice botas o polainas cuando se interne en la vegetación. No levante piedras o troncos o explore agujeros con las
+                    manos desnudas. Si se encuentra con una serpiente, no la moleste y retírese. No
+                    trate de manipular a las serpientes. Si recolecta café u otros materiales vegetales,
+                    ponga mucha atención y observe bien antes de poner las manos en las plantas.
+                    Tenga mucho cuidado cuando se levante al baño o a tomar agua por la noche pues
+                    las serpientes podrían estar cerca de su cama o en el camino hacia el baño o letrina. Difunda en su comunidad la información contenida en esta guía de identificación de especies de serpientes.
+                </p>
+                <h1 className="cueg-title">QUÉ HACER EN CASO DE MORDEDURA</h1>
+                <p className="cueg-paragraph">Mantenga la calma. Trate de inmovilizar el miembro mordido ya que los movimientos musculares favorecen la diseminación del veneno. Traslade INMEDIATAMENTE a la persona mordida al hospital o centro de salud más cercano. Las personas en alto riesgo deberían saber cuál es el hospital más cercano que dispone de
+                    suficientes dosis de antídoto. NO ES CONVENIENTE aplicar el suero antiofídico
+                    en condiciones de campo ya que podrían producirse reacciones adversas que son
+                    tratadas con mayor probabilidad de éxito en un hospital.
+                    No es beneficioso realizar torniquetes, succionar el veneno, o aplicar electricidad
+                    o pócimas de plantas. No se ha demostrado que estas medidas funcionen y sólo
+                    podrían causar más daño al área afectada.
+                </p>
+
+                
             </section>
         </>
         
