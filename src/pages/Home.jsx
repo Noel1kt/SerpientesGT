ac71fe27cc572a6be003d4004b3b125bcdbf6768
Noel1kt <3017297780101@ingenieria.usac.edu.gt>
2023-10-31 23:43:48 -0600

    home text

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,9 +16,7 @@ export const Home = () => {
         ></iframe>
       </div>
       <div className='text-conap'>
-        <h3>El Día Mundial de las Serpientes es una oportunidad para crear conciencia sobre la importancia de estos animales en el ecosistema.
-           Conservar y proteger las serpientes es importante por varias razones. En primer lugar, las serpientes son controladoras de plagas, ayudando a mitigar las poblaciones de ratones, insectos y otros animales que pueden afectar el desarrollo de la sociedad. Además, las serpientes constituyen una porción significativa de los depredadores, que mantienen a nuestros ecosistemas naturales equilibrados. También pueden controlar brotes de enfermedades. Esperamos que esta celebración ayude a crear conciencia sobre la importancia de las serpientes y su papel en el ecosistema.
-           ¡Valoremos nuestra Guatemala Megadiversa! Consejo Nacional de Áreas Protegidas ¡CONAP!</h3>
+        <h3></h3>
       </div>
     </div>
     
